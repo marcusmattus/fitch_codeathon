9a4eac0c175340173843c72969e8dc961ad2e33a
AnishKochhar <anish@kochhars.com>
2023-11-02 11:40:45 +0000

    Initial Commit

---
+++ FriendsOfBulgaria/FriendsOfBulgariaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsOfBulgariaApp.swift
+//  FriendsOfBulgaria
+//
+//  Created by Anish Kochhar on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsOfBulgariaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
